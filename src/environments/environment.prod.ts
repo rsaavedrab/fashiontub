505b21c8bb64a8593e0ee6705a8a3b1debb6bc61
Ronald Saavedra <ronaldsaavedra@macbook-pro.home>
2020-03-03 22:27:07 -0500

    dsdsd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
 	production: true,
 	isMockEnabled: true, // You have to switch this, when your real back-end is done
-	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5'
+	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
+	  login: '/login',
+	  //host: 'http://localhost:3600/'
+	  host: 'https://apinodefashiontub.herokuapp.com/'
 };
